413bf92396d86e3c8f5d7a060447a16963b849ae
lunatyq <maciej@lslabs.eu>
2016-01-13 17:29:04 +0100

    Init BZP

---
+++ spec/factories.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do  factory :publication do
+    biuletyn 1
+pozycja 1
+data_publikacji "2016-01-13"
+nazwa "MyString"
+ulica "MyString"
+nr_domu "MyString"
+nr_miesz "MyString"
+miejscowosc "MyString"
+kod_poczt "MyString"
+wojewodztwo "MyString"
+tel "MyString"
+fax "MyString"
+regon "MyString"
+e_mail "MyString"
+ogloszenie "MyString"
+przedmiot_zam "MyString"
+  end
+
+end
