f0be03d50113dc1d83d56c60bb505bb827fbb5f0
lunatyq <maciej@lslabs.eu>
2017-01-11 22:05:01 +0100

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922182521) do
+ActiveRecord::Schema.define(version: 20170111202236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160922182521) do
     t.datetime "imported_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "version"
   end
 
   add_index "archives", ["published_on", "year"], name: "index_archives_on_published_on_and_year", unique: true, using: :btree
